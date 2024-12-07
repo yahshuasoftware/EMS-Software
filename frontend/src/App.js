4a804712f052d38e4613e0e45411b6a76f750933
unknown <priyankanandi25@gmail.com>
2024-12-07 12:50:48 +0530

    n

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CRM Software <code>src/App.js</code> and save to reload.
+          CRM  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

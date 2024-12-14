202465038879e64aed03b454a31e682768a22f86
unknown <priyankanandi25@gmail.com>
2024-12-14 18:28:33 +0530

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,17 @@ import cors from 'cors';
 const app = express()
 import authRouter from "./routes/auth.js"
 import departmentRouter from "./routes/department.js"
+import employeeRouter from "./routes/employee.js"
 import 'dotenv/config'
 import connectToDatabase from './config/database.js';
 
 connectToDatabase()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public/uploads'))
 app.use('/api/auth',authRouter)
 app.use('/api/department',departmentRouter)
+app.use('/api/employee',employeeRouter)
 
 app.listen(
     process.env.PORT,

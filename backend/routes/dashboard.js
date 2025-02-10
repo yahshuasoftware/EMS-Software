0565309b1257f814a4d9f7571810b0519a5c15bc
Yahshua <yahshuasoftware@gmail.com>
2025-02-10 22:00:57 +0530

    Delete backend directory

--- backend/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import authMiddleware from '../middleware/authMiddleware.js'
-import { getSummary } from "../controllers/dashboardController.js"
-const router=express.Router()
-
-router.get('/summary',authMiddleware,getSummary)
-
-export default router;
--- backend/routes/setting.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import authMiddleware from '../middleware/authMiddleware.js'
-import { changePassword } from '../controllers/settingController.js'
-
-const router=express.Router()
-
-router.put('/change-password',authMiddleware,changePassword)
-
-export default router
